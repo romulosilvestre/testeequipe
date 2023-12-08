5258ff97da2ce99f55be8c794e0edaf7b1acccb1
Tue_GBS <mateusdasilva72@outlook.com>
2023-12-08 14:03:25 -0300

    SA-48

---
+++ sispanificacao/Models/TipoUsuario.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sispanificacao.Models
+{
+    public class TipoUsuario
+    {
+        public int ID {get; set; }
+        public string? Nome { get; set; }
+    }
+}
